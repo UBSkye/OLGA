0acdfb8e940c67e4d16b0dfecbfd0532b62f52a2
UncleBones <44440485+UncleBones@users.noreply.github.com>
2018-10-24 10:41:32 -0500

    Update pl.lua

--- pl.lua
+++ pl.lua
@@ -1,5 +1,8 @@
 pl={
   name="Olga",
+  health=8,
+  health_max=8,
+  
   wep="golden_staff",
   
   -- 0 = Not Found, 1 = Found & Disabled, 2 = Found & Enabled
@@ -17,7 +20,9 @@ pl={
     
       claw_of_the_maw=0, --Whip forward with “E”, then pull yourself in with “E”.
                          --Press E to throw, E to reel in, and E to cut early
-                         --When pulling in you can move left or right to slide that way, cutting will launch you ahead in the direction you’re facing
- 
+                         --When pulling in you can move left or right to slide that way, cutting will launch you ahead in the direction 
+                         --you’re facing
   },
+  
+  
 }
