7a03fab92a74ba84736ebc58a621dcdac419c2c4
UncleBones <44440485+UncleBones@users.noreply.github.com>
2018-10-24 10:39:10 -0500

    Update pl.lua

--- pl.lua
+++ pl.lua
@@ -1,3 +1,23 @@
 pl={
   name="Olga",
+  wep="golden_staff",
+  
+  -- 0 = Not Found, 1 = Found & Disabled, 2 = Found & Enabled
+  upgrades={
+      sceptre_pebble=0, --Shoot an orb of energy with each swing
+      triorb=0, --Now shoots three blasts out per swing
+      focus_charm=0, --Steadying the staff will enable a powerful blast and swing
+      doubled_thread=0, --Charge now fires full power twice for one
+    
+      vacuous_cloak=0, --Press “Q” to phase ahead
+                       --Phasing enables you to quickly dodge away from danger, but you can only go forward
+                       --Attempting to phase over a pit will cause you to fall
+      void_dust=0, --You may now phase through dark barriers and across gaps
+      voids_eye=0, --Your cooldown for phasing is significantly reduced
+    
+      claw_of_the_maw=0, --Whip forward with “E”, then pull yourself in with “E”.
+                         --Press E to throw, E to reel in, and E to cut early
+                         --When pulling in you can move left or right to slide that way, cutting will launch you ahead in the direction you’re facing
+ 
+  },
 }
